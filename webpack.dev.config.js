e1d310da787353edb32f4fb40e816d6d80d81484
haneric <haneric1986@gmail.com>
2017-02-02 16:00:32 +0800

    Support multiple entries and bootstrap v3

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,82 +1,62 @@
 const webpack = require('webpack');
 const path = require('path');
 const autoprefixer = require('autoprefixer');
-const bootstrapEntryPoints = require('./webpack.bootstrap.config.js');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-console.log(`=> bootstrap-loader configuration: ${bootstrapEntryPoints.dev}`);
 
 module.exports = {
 
-  entry: [
-    'webpack-dev-server/client',
-    'webpack/hot/dev-server',
-    'tether',
-    'font-awesome-loader',
-    bootstrapEntryPoints.dev,
-    './app/scripts/app',
-  ],
+  entry: {
+    bs3: [
+      'webpack-dev-server/client',
+      'webpack/hot/dev-server',
+      'font-awesome-loader',
+      `bootstrap-loader/lib/bootstrap.loader?configFilePath=${__dirname}/bs3.yml!bootstrap-loader/no-op.js`,
+      './app/scripts/app',
+    ],
+  },
 
   devServer: {
-    hot: true
+    hot: true,
   },
 
   output: {
-    path: path.join(__dirname, 'public', 'assets'),
-    filename: 'app.js',
+    path: path.join(__dirname, 'public'),
+    filename: '[name].js',
     publicPath: '/',
   },
 
 
-  resolve: { extensions: [ '', '.js' ] },
+  resolve: { extensions: ['', '.js'] },
 
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-      template: './app/markup/bootstrap-example.html'
+      template: './app/markup/bootstrap-example.html',
     }),
     new webpack.NoErrorsPlugin(),
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-      "window.jQuery": "jquery",
-      Tether: "tether",
-      "window.Tether": "tether",
-      Tooltip: "exports?Tooltip!bootstrap/js/dist/tooltip",
-      Alert: "exports?Alert!bootstrap/js/dist/alert",
-      Button: "exports?Button!bootstrap/js/dist/button",
-      Carousel: "exports?Carousel!bootstrap/js/dist/carousel",
-      Collapse: "exports?Collapse!bootstrap/js/dist/collapse",
-      Dropdown: "exports?Dropdown!bootstrap/js/dist/dropdown",
-      Modal: "exports?Modal!bootstrap/js/dist/modal",
-      Popover: "exports?Popover!bootstrap/js/dist/popover",
-      Scrollspy: "exports?Scrollspy!bootstrap/js/dist/scrollspy",
-      Tab: "exports?Tab!bootstrap/js/dist/tab",
-      Tooltip: "exports?Tooltip!bootstrap/js/dist/tooltip",
-      Util: "exports?Util!bootstrap/js/dist/util",
-    })
   ],
 
   module: {
     loaders: [
-      { test: /\.css$/, loaders: [ 'style', 'css', 'postcss' ] },
-      { test: /\.scss$/, loaders: [ 'style', 'css', 'postcss', 'sass' ] },
+      { test: /\.css$/, loaders: ['style', 'css', 'postcss'] },
+      { test: /\.scss$/, loaders: ['style', 'css', 'postcss', 'sass'] },
       {
         test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "url?limit=10000"
+        loader: 'url?limit=10000',
       },
       {
         test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-        loader: 'file'
+        loader: 'file',
       },
 
       { test: /\.html$/, loader: 'raw-loader' },
 
-      // Bootstrap 4
-      { test: /bootstrap\/dist\/js\/umd\//, loader: 'imports?jQuery=jquery' },
+      // Bootstrap 3
+      { test: /bootstrap-sass\/assets\/javascripts\//, loader: 'imports-loader?jQuery=jquery' },
     ],
   },
 
-  postcss: [ autoprefixer ],
+  postcss: [autoprefixer],
 
 };
